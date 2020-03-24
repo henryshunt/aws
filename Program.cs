ee4130c0d5fa9d82d2f968c50beb4aa46ebb6f8f
Henry Hunt <henryhunt.dev@gmail.com>
2020-03-24 15:48:54 +0000

    Added initial project files

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AWS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
