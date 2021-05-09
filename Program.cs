033e3cd755c686359d89eafab723b3b2b162bf12
Henry Hunt <henryhunt.dev@gmail.com>
2021-05-09 01:00:37 +0100

    Added a Linux platform check before startup

--- Program.cs
+++ Program.cs
@@ -1,9 +1,18 @@
-﻿namespace Aws
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Aws
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            if (!RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                Console.WriteLine("Only the Linux platform is supported");
+                return;
+            }
+
             new Core.Controller().Startup();
         }
     }
